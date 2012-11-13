fb6c63d5be9546c47f4b5ca900395f90ecb510cb
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2012-11-13 00:51:21 -0800

    commit

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import os
 from flask import Flask
 
+#----------------------------------------
+# initialization
+#----------------------------------------
+
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    import schedule.py
+app.config.update(
+    DEBUG = True,
+)
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+#----------------------------------------
+# controllers
+#----------------------------------------
+
+@app.route("/")
+def hello():
+    return "Hello from Python!"
+
+#----------------------------------------
+# launch
+#----------------------------------------
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
