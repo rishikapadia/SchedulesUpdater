1592fa27ee63b34522abb715618b777ddb2300a3
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2012-11-13 00:38:03 -0800

    init

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def main():
+    import schedule.py
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
