93733721cc7f78a26b1796f27926e0504a777c24
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2013-01-18 23:18:14 -0800

    removed math 1b lec 002

--- data-2.py
+++ data-2.py
@@ -1,1 +1,1 @@
-{'+15622918691': {('SPRING', 'PHYSICS', '7A', '001'): 'enrolled: 220/220, No waitlist', ('SPRING', 'PHYSICS', '7A', '107'): 'enrolled: 20/20, No waitlist', ('SPRING', 'MATH', '1B', '002'): 'enrolled: 314/325, waitlisted: 41/50'}}
+{'+15622918691': {('SPRING', 'PHYSICS', '7A', '001'): 'enrolled: 220/220, No waitlist', ('SPRING', 'PHYSICS', '7A', '107'): 'enrolled: 20/20, No waitlist'}}
