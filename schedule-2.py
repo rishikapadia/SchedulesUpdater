e123d5bae473381bbe691e4e94457cd6a5e3b951
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2013-03-01 10:29:09 -0800

    Update schedule-2.py

--- schedule-2.py
+++ schedule-2.py
@@ -15,21 +15,21 @@ from datetime import datetime, timedelta
 from email.utils import parsedate
 
 
-TWILIO_PHONE_NUMBER = "(562) 352 - 0309"
-TWILIO_ACCOUNT_SID = 'ACdd9b3407c0ff20c450ebe385bc09c71a'
-TWILIO_AUTH_TOKEN = '0a1ffeb8651886b31fe61bcd1ee3d47f'
+TWILIO_PHONE_NUMBER = "*"
+TWILIO_ACCOUNT_SID = '*'
+TWILIO_AUTH_TOKEN = '*'
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-FROM_NUMBER = '+15623520309'     # Twilio phone number
-TO_NUMBER = '+15622918691'     # Default TO_NUMBER
+FROM_NUMBER = '+*'     # Twilio phone number
+TO_NUMBER = '+*'     # Default TO_NUMBER
 
 urls = {'FALL': 'http://schedule.berkeley.edu/srchfall.html', 'SPRING': 'http://schedule.berkeley.edu/srchsprg.html', 'SUMMER': 'http://schedule.berkeley.edu/srchsmr.html'}
 abbrev = {"FALL":"FA", "SPRING":"SP", "SUMMER":"SU"}
 errors = ["Sorry, that is not a valid semester entry.", "Sorry, that is not a valid course entry.", "Sorry, that is not a valid section number.", "I'm sorry, you are not authorized to use this service.", "Telebears is down for an unscheduled maintenance.", "Section number returns too many results."]
 
-ADMIN = '+15622918691'
-AUTHORIZED = ['+15622918691']
-schedules = {'+15622918691': {}} # keys are phone numbers, values are dictionaries, of key msg_body(a list) and value class status
+ADMIN = '+*'
+AUTHORIZED = ['+*']
+schedules = {'+*': {}} # keys are phone numbers, values are dictionaries, of key msg_body(a list) and value class status
 
 global critical
 critical = False
